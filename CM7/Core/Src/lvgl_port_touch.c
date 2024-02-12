34b820ff1a09aae33212fdb252f6ea8e72469854
Lukasz Skalski <contact@lukasz-skalski.com>
2024-02-12 15:33:17 +0100

    Fix touchscreen IRQ handler
    Based on: https://github.com/lvgl/lv_stm/commit/b7fc3b8076ce39a65aaab85c6b49cf36140b81e1

--- CM7/Core/Src/lvgl_port_touch.c
+++ CM7/Core/Src/lvgl_port_touch.c
@@ -11,6 +11,9 @@
  **********************/
 
 static volatile uint8_t touch_irq = 0;
+static volatile lv_coord_t last_x = 0;
+static volatile lv_coord_t last_y = 0;
+static volatile lv_indev_state_t last_state = LV_INDEV_STATE_RELEASED;
 
 /**********************
  *  STATIC PROTOTYPES
@@ -54,44 +57,44 @@ static void
 lvgl_touchscreen_read (lv_indev_drv_t  *indev,
                        lv_indev_data_t *data)
 {
-  static lv_coord_t last_x = 0;
-  static lv_coord_t last_y = 0;
-
-  /* save the pressed coordinates and the state*/
+  /* use the saved coordinates if there were an interrupt */
   if(touch_irq)
     {
-      HAL_StatusTypeDef status;
-      uint8_t rx_buf[16] = {0};
-
       /* reset interrupt flag */
       touch_irq = 0;
 
-      /* read x/y coordinates */
-      status = HAL_I2C_Mem_Read(&hi2c1, (0x41 << 1), 0x10, 1, rx_buf, sizeof(rx_buf), 100);
-      if (status == HAL_OK)
-        {
-          last_x = (rx_buf[3] & 0x0F) << 8 | rx_buf[2];
-          last_y = (rx_buf[5] & 0x0F) << 8 | rx_buf[4];
-          data->state = LV_INDEV_STATE_PRESSED;
-        }
-	  else
-        {
-          data->state = LV_INDEV_STATE_RELEASED;
-        }
+      data->point.x = last_x;
+      data->point.y = last_y;
+      data->state = last_state;
     }
   else
     {
-      data->state = LV_INDEV_STATE_RELEASED;
+      last_state = LV_INDEV_STATE_RELEASED;
     }
-
-  /* set the last pressed coordinates */
-  data->point.x = last_x;
-  data->point.y = last_y;
 }
 
 void
 HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   if (GPIO_Pin == CTP_INT_Pin)
-    touch_irq = 1;
+    {
+      HAL_StatusTypeDef status;
+      uint8_t rx_buf[16] = {0};
+
+      touch_irq = 1;
+
+      /* read x/y coordinates */
+      status = HAL_I2C_Mem_Read (&hi2c1, (0x41 << 1), 0x10, 1,
+                                 rx_buf, sizeof(rx_buf), 100);
+      if (status == HAL_OK)
+        {
+          last_x = (rx_buf[3] & 0x0F) << 8 | rx_buf[2];
+          last_y = (rx_buf[5] & 0x0F) << 8 | rx_buf[4];
+          last_state = LV_INDEV_STATE_PRESSED;
+        }
+      else
+        {
+          last_state = LV_INDEV_STATE_RELEASED;
+        }
+    }
 }
